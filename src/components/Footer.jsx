d2e6c6ab5dbb46769c1d8745597254d224f19400
Tanjilul Islam <tanjilulislam37@gmail.com>
2025-11-11 19:13:47 +0600

    footer designed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,59 @@
-import React from 'react';
+import { Link } from "react-router";
 
 const Footer = () => {
-    return (
-        <div>
-            <h2>Footer</h2>
-        </div>
-    );
+  return (
+    <div>
+      <footer className="footer sm:footer-horizontal bg-base-200 text-base-content p-10">
+        <aside>
+          <Link
+            to="/"
+            className="text-2xl font-bold text-indigo-600 hover:text-indigo-700"
+          >
+            TheBookHaven
+          </Link>
+          <p>
+            ACME Industries Ltd.
+            <br />
+            Providing reliable tech since 1992
+          </p>
+        </aside>
+        <nav>
+          <h6 className="footer-title">Services</h6>
+          <a className="link link-hover">Branding</a>
+          <a className="link link-hover">Design</a>
+          <a className="link link-hover">Marketing</a>
+          <a className="link link-hover">Advertisement</a>
+        </nav>
+        {/* <nav>
+          <h6 className="footer-title">Company</h6>
+          <a className="link link-hover">About us</a>
+          <a className="link link-hover">Contact</a>
+          <a className="link link-hover">Jobs</a>
+          <a className="link link-hover">Press kit</a>
+        </nav> */}
+        <nav>
+          <h6 className="footer-title">Legal</h6>
+          <a className="link link-hover">Terms of use</a>
+          <a className="link link-hover">Privacy policy</a>
+          <a className="link link-hover">Cookie policy</a>
+        </nav>
+        <form>
+          <h6 className="footer-title">Newsletter</h6>
+          <fieldset className="w-80">
+            <label>Enter your email address</label>
+            <div className="join">
+              <input
+                type="text"
+                placeholder="username@site.com"
+                className="input input-bordered join-item"
+              />
+              <button className="btn btn-primary join-item">Subscribe</button>
+            </div>
+          </fieldset>
+        </form>
+      </footer>
+    </div>
+  );
 };
 
 export default Footer;
